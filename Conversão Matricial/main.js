13792330fc6474727aef164e10282ce2ead51aa9
liviacanuto <canutto.livia@gmail.com>
2024-11-04 22:17:42 -0300

    add: algoritmo de bresenham

---
+++ Conversão Matricial/main.js
@@ -0,0 +1,158 @@
+function main() {
+    const canvas = document.querySelector("#canvas");
+    const gl = canvas.getContext("webgl", { preserveDrawingBuffer: true });
+
+    if (!gl) {
+        throw new Error("WebGL not supported");
+    }
+
+    var vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
+    var fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
+
+    var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+    var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+    var program = createProgram(gl, vertexShader, fragmentShader);
+
+    gl.useProgram(program);
+
+    const positionBuffer = gl.createBuffer();
+    const colorBuffer = gl.createBuffer();
+
+    const positionLocation = gl.getAttribLocation(program, `position`);
+    gl.enableVertexAttribArray(positionLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+
+    const matrixUniformLocation = gl.getUniformLocation(program, `matrix`);
+    const colorUniformLocation = gl.getUniformLocation(program, `color`);
+
+    let matrix = [2 / canvas.width, 0, 0, 0, 0, -2 / canvas.height, 0, 0, 0, 0, 0, 0, -1, 1, 0, 1];
+    gl.uniformMatrix4fv(matrixUniformLocation, false, matrix);
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    let colorVector = [0.0, 0.0, 0.0];
+    gl.uniform3fv(colorUniformLocation, colorVector);
+
+    canvas.addEventListener("mouseup", mouseClick, false);
+    let x1 = 0,
+        y1 = 0,
+        x2 = 0,
+        y2 = 0;
+    function mouseClick(event) {
+        console.log("p1: " + x1 + " " + y1);
+        console.log("p2: " + x2 + " " + y2);
+        if (!x1) {
+            x1 = event.offsetX;
+            y1 = event.offsetY;
+        } else if (x1 && !x2) {
+            x2 = event.offsetX;
+            y2 = event.offsetY;
+            bresenham(x1, y1, x2, y2);
+        }
+    }
+
+    // const bodyElement = document.querySelector("body");
+    // bodyElement.addEventListener("keydown", keyDown, false);
+
+    function drawPoint(x, y) {
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([x, y]), gl.STATIC_DRAW);
+        gl.drawArrays(gl.POINTS, 0, 1);
+    }
+
+    function bresenham(x1, y1, x2, y2) {
+        if (x1 > x2) {
+            bresenham(x2, y2, x1, y1);
+            return;
+        }
+        const dx = x2 - x1;
+        const dy = y2 - y1;
+        let p = 2 * dy - dx;
+        const incInf = 2 * dy;
+        const incSup = 2 * (dy - dx);
+        let x = x1;
+        let y = y1;
+        drawPoint(x, y);
+
+        while (x < x2) {
+            if (p < 0) {
+                p = p + incInf;
+            } else {
+                p = p + incSup;
+                y++;
+            }
+            x++;
+            drawPoint(x, y);
+        }
+    }
+}
+
+function keyDown(event) {
+    switch (event.key) {
+        case "0":
+            colorVector = [0.0, 0.0, 0.0];
+            break;
+        case "1":
+            colorVector = [1.0, 0.0, 0.0];
+            break;
+        case "2":
+            colorVector = [0.0, 1.0, 0.0];
+            break;
+        case "3":
+            colorVector = [0.0, 0.0, 1.0];
+            break;
+        case "4":
+            colorVector = [1.0, 1.0, 0.0];
+            break;
+        case "5":
+            colorVector = [0.0, 1.0, 1.0];
+            break;
+        case "6":
+            colorVector = [1.0, 0.0, 1.0];
+            break;
+        case "7":
+            colorVector = [1.0, 0.5, 0.5];
+            break;
+        case "8":
+            colorVector = [0.5, 1.0, 0.5];
+            break;
+        case "9":
+            colorVector = [0.5, 0.5, 1.0];
+            break;
+    }
+    gl.uniform3fv(colorUniformLocation, colorVector);
+    drawPoint();
+}
+
+function createShader(gl, type, source) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+        return shader;
+    }
+
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+}
+
+main();
